6e51e036d3b10a2d209212bcf66d3abd33ad6594
KFL <kfldev@outlook.com>
2017-01-08 17:31:29 -0800

    working on nuspec

---
+++ RELEASE_NOTES.md
@@ -0,0 +1,3 @@
+## New in 1.0.0 (Released 2017/01/08)
+* Support rendering Markdown
+* Support extracting Title and Description
--- build.fsx
+++ build.fsx
@@ -2,19 +2,27 @@
 #r "./packages/FAKE/tools/FakeLib.dll"
 
 open Fake
+open Fake.NuGetHelper
+let dependencies = 
+    Fake.Paket.GetDependenciesForReferencesFile "./picdoc/paket.references"
+    |> List.ofSeq
 
 // Directories
 let buildDir  = "./build/"
 let deployDir = "./deploy/"
 
+let release = ReadFile "./RELEASE_NOTES.md" |> ReleaseNotesHelper.parseReleaseNotes
+
+let authors = ["KL"]
+let projectName = "picdoc"
+let projectDescription = "Generate markdown by extracting EXIF data from images"
+let projectSummary = "picdoc"
+let version = release.AssemblyVersion
 
 // Filesets
 let appReferences  =
     !! "/**/*.csproj"
     ++ "/**/*.fsproj"
-
-// version info
-let version = "0.1"  // or retrieve from CI server
 
 // Targets
 Target "Clean" (fun _ ->
@@ -27,16 +35,28 @@ Target "Build" (fun _ ->
     |> Log "AppBuild-Output: "
 )
 
-Target "Deploy" (fun _ ->
-    !! (buildDir + "/**/*.*")
-    -- "*.zip"
-    |> Zip buildDir (deployDir + "ApplicationName." + version + ".zip")
-)
+Target "CreatePackage" (fun _ ->
+    // Copy all the package files into a package folder
+    //!! "./build/*" |> CopyFiles deployDir
+
+    NuGetPack (fun p -> 
+            {p with
+                Authors = authors
+                Project = projectName
+                Description = projectDescription
+                OutputPath = deployDir
+                Summary = projectSummary
+                WorkingDir = deployDir
+                Files = [ ("../build/*", None, None) ]
+                Dependencies = dependencies 
+                Version = version }) 
+                "picdoc.nuspec")
 
 // Build order
 "Clean"
   ==> "Build"
-  ==> "Deploy"
+  ==> "CreatePackage"
 
 // start build
-RunTargetOrDefault "Build"
+//RunTargetOrDefault "Build"
+RunTargetOrDefault "CreatePackage"

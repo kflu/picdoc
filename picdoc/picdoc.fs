693f9b5c45f2f4e2aa851383391a1f9e01a496c2
KFL <kfldev@outlook.com>
2017-01-08 16:30:16 -0800

    code complete

--- picdoc/picdoc.fs
+++ picdoc/picdoc.fs
@@ -3,106 +3,49 @@ module picdoc
 #if INTERACTIVE
 #r "../packages/MetadataExtractor/lib/net45/MetadataExtractor.dll"
 #r "../packages/XmpCore/lib/net35/XmpCore.dll"
+#r "../packages/Nustache/lib/net20/Nustache.Core.dll"
 #endif
 
-open MetadataExtractor
+open System
+open System.IO
 open Argu
+open MetadataExtractor
 
-type ImageInfo = {
-    Title : string
-    Description: string
-    Path : string
-    Tags : Tag array
-}
-with 
-    static member Default = 
-        { Title = ""; Description = ""; Path = ""; Tags = Array.empty<Tag> }
+open core
+open core.utils
+open renderer
 
 type Options =
-    | InputDir of path:string
-    | FilePattern of pattern:string
+    | [<MainCommand; ExactlyOnce>] InputDir of path:string
+    | [<AltCommandLine("-p")>] FilePattern of pattern:string
+    | [<AltCommandLine("-l")>] LinkPrefix of prefix:string
 with
     interface IArgParserTemplate with
-        member s.Usage = ""
-
-let getTags (fn:string) =
-    ImageMetadataReader.ReadMetadata(fn)
-    |> Seq.collect (fun (d: Directory) -> d.Tags)
-    |> Array.ofSeq
-
-let toLower (x:string) = x.ToLower()
-
-let titles = ["Windows XP Title"] |> List.map toLower
-
-let descriptions = ["Windows XP Comment"] |> List.map toLower
-
-let getField (picks: string list) (tags: Tag seq) : (string option) =
-    tags 
-    |> Seq.tryFind 
-        (fun t -> List.contains (t.Name.ToLower()) picks)
-    |> Option.map (fun t -> t.Description)
-
-let extract f info tags = f info tags
-
-let getTitle info = 
-    match getField titles (info.Tags) with
-    | None -> info
-    | Some(v) -> {info with Title = v}
-
-let getDesc info = 
-    match getField descriptions (info.Tags) with
-    | None -> info
-    | Some(v) -> {info with Description = v}
-
-module Markdown =
-
-    // FIXME content needs to be escaped
-    let renderOne (info: ImageInfo) =
-        sprintf """
-**%s**
-
-%s
-
-![%s](%s)""" (info.Title) info.Description info.Title info.Path
-
-    let renderAll (infos: ImageInfo seq) =
-        [ for i in infos do
-                yield renderOne i 
-                yield ""
-        ]
-        |> String.concat (System.Environment.NewLine)
-
-open System.IO
-open System
-
-
-let GetRelativePath (filespec: string) (folder :string) =
-    let mutable folderUri = new Uri(filespec);
-    // Folders must end in a slash
-    let mutable folder = folder
-    if not (folder.EndsWith(Path.DirectorySeparatorChar.ToString())) then 
-        folder <- folder + Path.DirectorySeparatorChar.ToString()
-    else folderUri <- new Uri(folder)
-    Uri.UnescapeDataString(folderUri.MakeRelativeUri(folderUri).ToString().Replace('/', Path.DirectorySeparatorChar))
+        member s.Usage =
+            match s with
+            | InputDir _ -> "input directory"
+            | FilePattern _ -> "pattern of files to be included"
+            | LinkPrefix _ -> "string to prefix the link to the images"
 
 [<EntryPoint>]
 let main argv =
-    let parser = ArgumentParser.Create<Options>(errorHandler=ProcessExiter(colorizer = function ErrorCode.HelpText -> None | _ -> Some ConsoleColor.Red))
+    let parser = ArgumentParser.Create<Options>(errorHandler=ProcessExiter())
     let opts = parser.Parse(argv)
 
-    let relativeTo = System.Environment.CurrentDirectory
     let targetDir = opts.GetResult(<@ InputDir @>) |> Path.GetFullPath
-    let filePattern = opts.GetResult(<@ FilePattern @>)
-    printfn "in: %s pat: %s" targetDir filePattern
-    let rendered = 
-        System.IO.Directory.GetFiles(targetDir, filePattern)
+    let filePattern = opts.GetResult(<@ FilePattern @>, defaultValue="*")
+    let prefix = opts.GetResult(<@ LinkPrefix @>, defaultValue="")
+
+    let infos = 
+        System.IO.Directory.GetFiles(targetDir)
         |> Array.map 
-            (fun fn -> 
-                { ImageInfo.Default with 
-                    Path = fn // FIXME: this doesn't work: GetRelativePath (Path.GetFullPath(fn)) targetDir
-                    Tags = getTags fn })
-        |> Array.map (getTitle >> getDesc)
-        |> Markdown.renderAll
+            (fun fn -> { ImageInfo.Default with Path = fn |> Path.GetFullPath; Tags = getTags fn })
+        |> Array.map // set the default manipulators
+            (Manipulator.setImageUrl prefix
+             >> Manipulator.fromExtractor Extractor.Title
+             >> Manipulator.fromExtractor Extractor.Description)
+    
+    let rendered = Markdown.render infos // render
 
     printfn "%s" rendered
 
---
+++ picdoc/renderer.fs
@@ -0,0 +1,27 @@
+module renderer
+
+open Nustache.Core
+open core
+
+module Markdown =
+
+    let template = """
+{{#images}}
+![{{title}}]({{path}})
+
+{{#title}}
+**{{title}}**
+
+{{/title}}
+{{#description}}
+{{description}}
+
+{{/description}}
+{{/images}}
+"""
+    let createData (infos: ImageInfo seq) =
+        Map.ofList
+            [ "images", infos |> Seq.map (fun info -> info.Props) |> Array.ofSeq ]
+
+    let render (infos: ImageInfo seq) =
+        Nustache.Core.Render.StringToString(template, createData infos)

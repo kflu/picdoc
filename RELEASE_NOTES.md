d83aa15a1788bec9bce6a68ecb1d4aaf37321630
KFL <kfldev@outlook.com>
2017-01-11 21:45:23 -0800

    1.0.3

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,7 @@
+## New in 1.0.3 (Released 2017/01/11)
+* Added ilmerged finally executable (no external dependencies)
+* Simplified dependencies
+
 ## New in 1.0.2 (Released 2017/01/08)
 * Fix unicode rendering https://github.com/jdiamond/Nustache/issues/126
 
--- picdoc/AssemblyInfo.fs
+++ picdoc/AssemblyInfo.fs
@@ -7,8 +7,8 @@ open System.Runtime.InteropServices
 
 [<assembly: AssemblyTitle("picdoc")>]
 [<assembly: AssemblyDescription("Command line tool to generate markdown doc by extracting metadata from image files.")>]
-[<assembly: AssemblyVersion("1.0.2")>]
-[<assembly: AssemblyFileVersion("1.0.2")>]
-[<assembly: AssemblyInformationalVersion("1.0.2")>]
+[<assembly: AssemblyVersion("1.0.3")>]
+[<assembly: AssemblyFileVersion("1.0.3")>]
+[<assembly: AssemblyInformationalVersion("1.0.3")>]
 
 ()
